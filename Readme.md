ecb6b231ec381fc3feffc11bfcb9b417a274976c
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-04-28 17:02:08 +0600

    packages

--- Readme.md
+++ Readme.md
@@ -1,2 +1,11 @@
 Hlw!!
-backend with 
+backend with GO
+
+go mod init go-resm
+
+ go mod tidy    for intall packages if it missing there 
+
+Gin package : https://pkg.go.dev/github.com/gin-gonic/gin
+
+https://github.com/mongodb/mongo-go-driver
+go get go.mongodb.org/mongo-driver/v2/mongo
--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"go-resm/database"
+	"go-resm/middleware"
+	"go-resm/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var fooCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.UserRouters(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+
+}
