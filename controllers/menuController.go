44956c164d2cab6c4f8deb375acc11616d217715
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-05-05 20:01:24 +0600

    menu controller

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,9 +1,14 @@
 package controllers
 
 import (
+	"context"
 	"go-resm/database"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
@@ -11,7 +16,18 @@ var menuCollection *mongo.Collection = database.OpenCollection(database.Client, 
 
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing the menu"})
+		}
+		var allMenus []bson.M
 
+		if err = result.All(ctx, &allMenus); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenus)
 	}
 }
 
